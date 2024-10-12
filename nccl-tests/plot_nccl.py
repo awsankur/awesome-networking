f1c70d1f2e506cb65c9a3532d00a3b2d1f6f7e4c
Ankur Srivastava <101727556+awsankur@users.noreply.github.com>
2024-10-12 05:51:18 -0700

    Update plot_nccl.py

--- nccl-tests/plot_nccl.py
+++ nccl-tests/plot_nccl.py
@@ -61,7 +61,7 @@ fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(15, 15))
 axes_row_list = [0,0,1,1,2]
 axes_col_list = [0,1,0,1,0]
 
-c=0
+c = 0
 for one_node in nnodes:
 
     print(f'Processing NCCL Test for {one_node} nodes')
@@ -81,20 +81,17 @@ for one_node in nnodes:
 
     # Ignore anything beyond 25ms
     df = df[df['duration_ms'] < df['duration_ms'].quantile(0.99)]
+
+    ax1 = axes[axes_row_list[c],axes_col_list[c]]
     
-    df.plot(ax=axes[axes_row_list[c],axes_col_list[c]], y='duration_ms', 
-            use_index=True,linewidth=2, linestyle='--', marker='o')
+    df.plot(ax=ax1, y='duration_ms', use_index=True,linewidth=2, linestyle='--', marker='o')
     # Plot p99
     y_position = df['duration_ms'].quantile(0.99)
-    axes[axes_row_list[c],axes_col_list[c]].axhline(y = y_position, color='k', linestyle='dashed', linewidth=1)
-    axes[axes_row_list[c],axes_col_list[c]].set_title(f'nnodes = {one_node}')
+    ax1.axhline(y = y_position, color='k', linestyle='dashed', linewidth=1)
+    ax1.set_title(f'nnodes = {one_node}')
     # Add text on the horizontal line
-    ax1 = axes[axes_row_list[c],axes_col_list[c]]
     ax1.text(0.25, y_position, f'p99 latency = {y_position:.2f} ms', color='r', va='center', ha='center',
         transform=ax1.get_yaxis_transform(), bbox=dict(facecolor='white', alpha=0.8, edgecolor='none'))
-    
-    #axes[axes_row_list[c],axes_col_list[c]].set_title(f'nnodes = {one_node}')
-
     
 
     c=c+1
@@ -129,4 +126,45 @@ for one_node in nnodes:
 
 
 p_df = pd.DataFrame(list(zip(nnodes,p50_list,p75_list,p95_list,p99_list,pmax_list)),columns = ['nnodes','p50_ms','p75_ms','p95_ms','p99_ms','pmax_ms'])
+
+# Create subplots
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(15, 15))
+
+ax1 = axes[0,0]
+# List of colors
+colors = ['red', 'green', 'blue']
+
+# Plot each column as a separate line
+for i, column in enumerate(p_df.columns[1:]):
+    if i < 3:
+        ax1.plot(p_df['nnodes'], p_df[column], color=colors[i], 
+                 label=column, linewidth=2, linestyle='--', marker='o')
+
+# Add legend and labels
+ax1.legend()
+ax1.set_xlabel('Number of nodes', fontsize=10, fontweight='bold')
+ax1.set_ylabel('Latency in ms', fontsize=10, fontweight='bold')
+ax1.set_title('P5 All Reduce:Sum Message Size = 25 MB', fontsize=16, fontweight='bold')
+
+ax2 = axes[0,1]
+ax2.plot(p_df['nnodes'], p_df['p99_ms'], 'black', 
+                 label=column, linewidth=2, linestyle='--', marker='o')
+# Add legend and labels
+ax2.legend()
+ax2.set_xlabel('Number of nodes', fontsize=10, fontweight='bold')
+ax2.set_ylabel('Latency in ms', fontsize=10, fontweight='bold')
+ax2.set_title('P99 Latency', fontsize=16, fontweight='bold')
+
+ax2 = axes[1,0]
+ax2.plot(p_df['nnodes'], p_df['pmax_ms'], 'black', 
+                 label=column, linewidth=2, linestyle='--', marker='o')
+# Add legend and labels
+ax2.legend()
+ax2.set_xlabel('Number of nodes', fontsize=10, fontweight='bold')
+ax2.set_ylabel('Latency in ms', fontsize=10, fontweight='bold')
+ax2.set_title('Max Latency', fontsize=16, fontweight='bold')
+
+plt.tight_layout()
+plt.savefig(f'scaling_all_reduce_sum_25MB_P5.png')
+#plt.show()
     
