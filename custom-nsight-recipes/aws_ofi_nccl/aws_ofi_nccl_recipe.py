99474c8782eef19655dc74e2188dc03a1a9f3a0b
Ankur Srivastava <awsankur@amazon.com>
2024-11-20 13:02:30 -0800

    updated recipe
    Signed-off-by: Ankur Srivastava <awsankur@amazon.com>

--- custom-nsight-recipes/aws_ofi_nccl/aws_ofi_nccl_recipe.py
+++ custom-nsight-recipes/aws_ofi_nccl/aws_ofi_nccl_recipe.py
@@ -21,17 +21,17 @@ from nsys_recipe.log import logger
 from nsys_recipe.lib.table_config import CompositeTable
 from nsys_recipe.lib import helpers, pace, recipe, summary
 
+import matplotlib.pyplot as plt
+import os
+
 class AwsOfiNcclRecipe(recipe.Recipe):
     @staticmethod
     def _mapper_func(report_path, parsed_args):
 
         service = DataService(report_path, parsed_args)
 
-        # Get String Ids
-        service.queue_table("StringIds")
-
-        # Target Info??
-        #service.queue_table("TARGET_INFO_SESSION_START_TIME")
+        # Get Cuda GPU Kernel Data
+        service.queue_table("CUPTI_ACTIVITY_KIND_KERNEL", ["shortName", "start", "end", "deviceId"])
 
 
         # Get NVTX Data
@@ -42,10 +42,30 @@ class AwsOfiNcclRecipe(recipe.Recipe):
         if df_dict is None:
             return None
 
+        kernel_df = df_dict["CUPTI_ACTIVITY_KIND_KERNEL"]
+        service.filter_and_adjust_time(kernel_df)
+
+        kernel_df = data_utils.replace_id_with_value(
+            kernel_df, df_dict["StringIds"], "shortName", "name"
+        )
+
+        if kernel_df.empty:
+            logger.info(
+                f"{report_path}: Report was successfully processed, but no NCCL data was found."
+            )
+            return None
+
+        kernel_df["duration"] = kernel_df["end"] - kernel_df["start"]
+        kernel_df = kernel_df[kernel_df["duration"] > 0]
+
+
+        kernel_df = kernel_df_orig.sort_values(by='start')
+        nccldf = kernel_df[kernel_df.apply(lambda x: x.astype(str).str.contains('nccl', case=False).any(), axis=1)]
+
         nvtx_df = df_dict[CompositeTable.NVTX]
         service.filter_and_adjust_time(nvtx_df)
 
-        nvtx_df.to_csv('./nvtx_df.csv')
+        ##nvtx_df.to_csv('./nvtx_df.csv')
 
         if nvtx_df.empty:
             logger.info(
@@ -53,23 +73,17 @@ class AwsOfiNcclRecipe(recipe.Recipe):
             )
             return None
 
-
-        #kernel_df["duration"] = kernel_df["end"] - kernel_df["start"]
-        #kernel_df = kernel_df[kernel_df["duration"] > 0]
-
-
-        #stats_df = summary.describe_column(kernel_df.groupby(["name"])["duration"])
-        #stats_df.index.name = "Name"
-
-        #stats_by_device_df = summary.describe_column(
-        #    kernel_df.groupby(["name", "deviceId"])["duration"]
-        #)
-        #stats_by_device_df.index.names = ["Name", "Device ID"]
-
         filename = Path(report_path).stem
 
-        return filename, nvtx_df
-        #return filename, stats_df, stats_by_device_df
+        nccldf.to_parquet(self.add_output_file("nccldf.parquet"))
+        nvtx_df.to_parquet(self.add_output_file("nvtx_df.parquet"))
+
+        if self._parsed_args.csv:
+            nccldf.to_csv(self.add_output_file("nccldf.csv"))
+            nvtx_df.to_csv(self.add_output_file("nvtx_df.csv"))
+
+        return filename, nccldf, nvtx_df
+        
 
     @log.time("Mapper")
     def mapper_func(self, context):
@@ -81,12 +95,61 @@ class AwsOfiNcclRecipe(recipe.Recipe):
             )
         )
 
+    def get_domain_ids(self,mapper_res):
+        filtered_res = helpers.filter_none(mapper_res)
+        # Sort by file name.
+        filtered_res = sorted(filtered_res, key=lambda x: x[0])
+        filenames, nccldf, nvtx_df = zip(*filtered_res)
+
+        domain_df = nvtx_df.loc[nvtx_df['text'].str.contains('aws', case=False)==True,]
+
+        send_domain_ids = domain_df.loc[domain_df['text'].str.contains('s_comm')==True,'domainId'].to_list()
+
+        recv_domain_ids = domain_df.loc[domain_df['text'].str.contains('r_comm')==True,'domainId'].to_list()
+
+        domain_names = domain_df['text'].to_list()
+
+        aws_ofi_nccl_df = nvtx_df.loc[(nvtx_df['domainId']>=domain_Id_min)
+                                & (nvtx_df['domainId']<=domain_Id_max),]
+
+        
+        # Domain ID ==0 for Eagr_recv??
+        return (aws_ofi_nccl_df, domain_df, send_domain_ids, recv_domain_ids, domain_names)
+
+    def plot_send_events(aws_ofi_nccl_df, send_domain_ids, domain_names):
+
+
+        for i in send_domain_ids:
+
+            domain_name = domain_names[i-2]
+
+            os.makedirs(f'{self.get_output_dir()}/{domain_name}', exist_ok=True)
+
+            # Create subplots
+            fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(12, 10))
+            
+            one_send_df = aws_ofi_nccl_df.loc[(aws_ofi_nccl_df['domainId']==i) & 
+                                           (aws_ofi_nccl_df['text']=='Send'),]
+            
+            one_send_df = one_send_df.reset_index()
+
+            one_send_df['duration_micro_s'] = (one_send_df['end'] - one_send_df['start'])/1000
+            one_send_df.hist(column = 'duration_micro_s', ax = axes[0],density=True, bins=100, edgecolor='blue')
+            one_send_df.plot(ax = axes[1],y='duration_micro_s', use_index=True,linewidth=2, linestyle='--', marker='o')
+            plt.suptitle(f'Send event durations for {domain_name}', fontsize=16, fontweight='bold')
+            plt.tight_layout()
+            plt.savefig
+
+
+
+
+
     @log.time("Reducer")
     def reducer_func(self, mapper_res):
         filtered_res = helpers.filter_none(mapper_res)
         # Sort by file name.
         filtered_res = sorted(filtered_res, key=lambda x: x[0])
-        filenames, stats_dfs, stats_by_device_dfs = zip(*filtered_res)
+        filenames, nccldf, nvtx_df = zip(*filtered_res)
 
         files_df = pd.DataFrame({"File": filenames}).rename_axis("Rank")
         files_df.to_parquet(self.add_output_file("files.parquet"))
@@ -133,6 +196,8 @@ class AwsOfiNcclRecipe(recipe.Recipe):
         super().run(context)
 
         mapper_res = self.mapper_func(context)
+        domain_tuple = self.get_domain_ids(mapper_res)
+
         #self.reducer_func(mapper_res)
 
         #self.save_notebook()
